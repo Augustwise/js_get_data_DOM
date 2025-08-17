0a051876887e3bfd5d18335c07b1ee50aabf5b20
Mykola <nyr3mvneov@gmail.com>
2025-08-17 18:54:00 +0300

    Added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+let totalPopulation = 0;
+const populations = [];
+
+population.forEach((node) => {
+  const value = Number(node.textContent.replace(/,/g, ''));
+
+  totalPopulation += value;
+  populations.push(value);
+});
+
+const totalPopulationElement = document.querySelector('.total-population');
+
+totalPopulationElement.textContent = totalPopulation.toLocaleString('en-US');
+
+const averagePopulation = totalPopulation / populations.length;
+
+const averagePopulationElement = document.querySelector('.average-population');
+
+averagePopulationElement.textContent =
+  Math.round(averagePopulation).toLocaleString('en-US');
